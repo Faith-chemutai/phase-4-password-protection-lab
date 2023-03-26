2e591e3ed0cd1660ce2e85e26f931416a88dba60
Faith <faithchemutai2330@gmail.com>
2023-03-26 22:03:54 +0300

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,33 +1,17 @@
-class UsersController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid, with: :render_record_invalid_response
-    
-      def create
-        user = User.create!(user_params)
-        if user
-          session[:user_id] = user.id
-          render json: user, status: :created
-        else
-          render json: { error: user.record.errors.full_messages }, status: :unprocessable_entity
-        end
-      end
-    
-      def show
-        user = User.find_by(id: session[:user_id])
-        if user
-          render json: user
-        else
-          render json: { error: "Not authorized" }, status: :unauthorized
-        end
-      end
-    
-      private
-    
-      def user_params
-        params.permit(:username, :password, :password_confirmation)
-      end
-    
-      def render_record_invalid_response(e)
-        render json: { error: e.record.errors.full_messages }, status: :unprocessable_entity
-      end
-    
+# in app/controllers/sessions_controller.rb
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { error: 'Invalid username or password' }, status: :unauthorized
     end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    head :no_content
+  end
+end
